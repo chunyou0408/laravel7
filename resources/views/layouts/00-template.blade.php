83b7b822f750bf5b9f747d5f3a00a6ae84291f3e
chunyou0408 <app520321@gmail.com>
2021-01-25 10:49:21 +0800

    Update 00-template.blade.php

--- resources/views/layouts/00-template.blade.php
+++ resources/views/layouts/00-template.blade.php
@@ -14,11 +14,15 @@
     {{-- swiper CDN --}}
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
     {{-- CSS --}}
+
+    {{-- bootstrap CDN --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
     @yield('css')
 </head>
 
 <body>
-    {{-- @yield('banner') --}}
+    @yield('banner')
     <section class="flex">
         <nav class="nav01">
             <div class="nav_logo_box">
@@ -314,6 +318,10 @@
     <script src="{{ asset('js/app.js') }}"></script>
     {{-- swiper --}}
     <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+    {{-- bootstrap --}}
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     {{-- JS --}}
     @yield('js')
 </body>

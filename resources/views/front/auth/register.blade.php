51f7950453f4db41b0a5ef75ef1c515ccf9913df
chunyou0408 <app520321@gmail.com>
2021-01-25 20:42:02 +0800

    0125

--- resources/views/front/auth/register.blade.php
+++ resources/views/front/auth/register.blade.php
@@ -6,19 +6,20 @@
 
 @section('main')
 
-<div class="register-title">
-    <span>註冊帳號</span>
-</div>  
-
-<section style="height: calc(100vh - 306px);" class="flex justify-content-center align-items-center">
+<section style="height: calc(100vh - 382px);" class="flex justify-content-center align-items-center">
     <div class="card-body">
         <form method="POST" action="{{ route('register') }}">
             @csrf
+            <div class="form-group row">
+                <div class="register-title">
+                    <span>註冊帳號</span>
+                </div>
+            </div>
 
             <div class="form-group row">
                 <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('姓名') }}</label>
 
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                     @error('name')
@@ -32,7 +33,7 @@
             <div class="form-group row">
                 <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('電子信箱') }}</label>
 
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                     @error('email')
@@ -46,7 +47,7 @@
             <div class="form-group row">
                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('密碼') }}</label>
 
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                     @error('password')
@@ -60,13 +61,13 @@
             <div class="form-group row">
                 <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('確認密碼') }}</label>
 
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                 </div>
             </div>
 
             <div class="form-group row mb-0">
-                <div class="col-md-6 offset-md-4">
+                <div class="col-md-4 offset-md-4">
                     <button type="submit" class="btn btn-primary">
                         {{ __('註冊') }}
                     </button>
--- resources/views/layouts/00-template.blade.php
+++ resources/views/layouts/00-template.blade.php
@@ -244,9 +244,9 @@
 
 
     <!-- Scripts -->
-    {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+    <script src="{{ asset('js/app.js') }}"></script>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> --}}
 
     {{-- swiper --}}
     <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>

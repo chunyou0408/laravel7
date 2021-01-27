090c58a32a739cedfa7233accc27ddbbe161238e
chunyou0408 <app520321@gmail.com>
2021-01-27 19:57:37 +0800

    0127

--- resources/views/front/01-index.blade.php
+++ resources/views/front/01-index.blade.php
@@ -52,8 +52,8 @@
       <div class="swiper-wrapper">
         <div class="swiper-slide" ><img src="./img/01/grassland.jpg" alt="" width="100%"></div>
         <div class="swiper-slide" ><img src="./img/01/tent.jpg" alt="" width="100%"></div>
-        <div class="swiper-slide" ><img src="./img/01/coffee_shop.jpg" alt="" width="100%"></div>
-        <div class="swiper-slide" ><img src="./img/01/night.jpg" alt="" width="100%"></div>
+        <div class="swiper-slide" ><img src="./img/01/coffee_shop.tiff" alt="" width="100%"></div>
+        <div class="swiper-slide" ><img src="./img/01/night.tiff" alt="" width="100%"></div>
 
       </div>
       <!-- Add Pagination -->
